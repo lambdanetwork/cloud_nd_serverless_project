01b1450cfa0fb8935fb797fc7afd428a6000f81c
alfredov@garena.com <alfredov@garena.com>
2020-11-19 21:08:51 +0700

    adding logs and getUserId to deleteTodo

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -2,16 +2,20 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import * as AWS from 'aws-sdk';
+import { createLogger } from '../../utils/logger';
+import { getUserId } from '../utils';
 AWS.config.update({region: 'ap-southeast-1'});
 
 const ddbDocumentClient = new AWS.DynamoDB.DocumentClient();
+const logger = createLogger('delete todo');
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent, context): Promise<APIGatewayProxyResult> => {
   try {
     let todoId: string, userId: string;
     if(event){
+      logger.info('context', context);
       todoId = event.pathParameters.todoId
-      userId = event.pathParameters.userId
+      userId = getUserId(event);
     } else {
       todoId = "123"
       userId = "user123"
@@ -25,14 +29,14 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       }
     };
 
-    ddbDocumentClient.delete(params).promise();
-
+    const result = await ddbDocumentClient.delete(params).promise();
+    logger.info(result)
   
     return {
       statusCode: 200,
       body: null
     }
   } catch(err){
-    console.error(err)
+    logger.error(err)
   }
 }
