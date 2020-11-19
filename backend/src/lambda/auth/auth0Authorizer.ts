5413fbdff967f4eba8b279f7447ba2dc4a40bf13
alfredov@garena.com <alfredov@garena.com>
2020-11-19 17:19:16 +0700

    update authentication information

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -57,12 +57,8 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
   const res = await Axios.get(jwksUrl);
 
-  console.log(jwt)
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
   const pemData = res['data']['keys'][0]['x5c'][0]
   const cert = `-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----`
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = '21wuod5dfc'
+export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-1.amazonaws.com/dev/todos`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'vidy-cloud-nd.au.auth0.com',            // Auth0 domain
+  clientId: 'XX7m35lqHQvhN1HWnCHKYhpPt7aFXPF9',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
